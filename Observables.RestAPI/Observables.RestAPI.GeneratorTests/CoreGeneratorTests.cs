7e585408a60eace67b4f372ab37491e522c18076
落笔wys <46271592+Skymly@users.noreply.github.com>
2026-06-06 20:41:21 +0800

    Wire RestAPI OBS3004 path template validation and OBS3005 on R3. (#65)
    1、Report OBS3004 when path placeholders do not match method parameters
    
    2、Report OBS3005 (not OBS3003) when R3 generator sees IObservable return types
    
    3、Add generator test for OBS3004
    
    Closes #60

--- Observables.RestAPI/Observables.RestAPI.GeneratorTests/CoreGeneratorTests.cs
+++ Observables.RestAPI/Observables.RestAPI.GeneratorTests/CoreGeneratorTests.cs
@@ -46,4 +46,25 @@ public class CoreGeneratorTests
 
         return Verifier.Verify(GeneratorTestHarness.ToSnapshot(output));
     }
+
+    [Fact]
+    public Task GetUser_path_parameter_mismatch_reports_OBS3004()
+    {
+        GeneratorRunOutput output = GeneratorTestHarness.Run(
+            """
+            public interface IUserApi
+            {
+                [Get("/users/{id}")]
+                Observable<User> GetUser(int id, int page);
+            }
+
+            public sealed class User
+            {
+                public int Id { get; set; }
+            }
+            """);
+
+        Assert.Contains("OBS3004", GeneratorTestHarness.ToSnapshot(output), StringComparison.Ordinal);
+        return Task.CompletedTask;
+    }
 }

257e1aa57538e2cd0052a2b9d79ff20209ebc79a
落笔wys <46271592+Skymly@users.noreply.github.com>
2026-09-12 19:06:30 +0800

    Use IdentifierHelper.BackingFieldName for keyword members

--- Observables.Postgres/Observables.Postgres.SourceGenerators.Shared/Emitter.cs
+++ Observables.Postgres/Observables.Postgres.SourceGenerators.Shared/Emitter.cs
@@ -49,9 +49,9 @@ internal static class Emitter
 
             writer.WriteLine(
                 $$"""
-                    private {{member.ReturnTypeDisplay}}? _{{member.MemberName}};
+                    private {{member.ReturnTypeDisplay}}? {{IdentifierHelper.BackingFieldName(member.MemberName)}};
                     public {{member.ReturnTypeDisplay}} {{member.MemberName}} =>
-                        _{{member.MemberName}} ??= {{listenCall}};
+                        {{IdentifierHelper.BackingFieldName(member.MemberName)}} ??= {{listenCall}};
 
                 """);
             return;
